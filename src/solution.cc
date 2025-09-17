a74f844bce2ae5d64f9f7c49549e822a86f06284
AdityaChandra316 <aditya.chandra123@outlook.com>
2025-09-17 18:51:12 -0500

    Update solution.cc

--- src/solution.cc
+++ src/solution.cc
@@ -1,6 +1,16 @@
 #include "solution.hpp"
+#include <fstream>
+#include <string>
 
 Student ReadStudentRecFromStream(std::istream& is) {
-  // your implementation here...
-  return Student{};
+  std::string firstName = "";
+  std::string lastName = "";
+  char waste = '\0';
+  unsigned int uin;
+  double gpa;
+  is << firstName << waste << lastName << waste << uin << gpa;
+  if (is.fail()) {
+    return Student{};
+  }
+  return Student{firstName, lastName, uin, gpa};
 }
